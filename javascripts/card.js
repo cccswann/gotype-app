6d1617ead0e36a38aa98adea1116afb1a726f7b7
Ciara Swann <cccswann@gmail.com>
2021-06-21 13:07:49 -0700

    Update Card constructor.

--- javascripts/card.js
+++ javascripts/card.js
@@ -8,11 +8,16 @@ class Card {
     this.word_base_list = object.word_base_list
     this.word_list = object.word_list
     this.deck_id = object.deck_id
+    this.translation_word_list = object.translation_word_list
+    this.translation_word_base_list = object.translation_word_base_list
+    this.translation = object.translation
+    this.image = object.image
+    this.sound = object.sound
 
     cardStore.push(this)
 }
 
-static getSentence(){
+static getCard(){
     fetch('http://localhost:3000/api/v1/cards')
     .then(response => response.json())
     .then(data => {
