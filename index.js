7b3e253d343f7d63d6fb0c001e3fd5091e1f8bab
Ciara Swann <cccswann@gmail.com>
2021-07-06 16:28:03 -0700

    Add new show score function.

---
+++ index.js
@@ -0,0 +1,457 @@
+const textDisplay = document.querySelector('#text-display');
+const inputField = document.querySelector('#input-field');
+
+let typingMode = 'wordcount';
+let wordCount;
+let timeCount;
+
+let randomWords = [];
+let wordList = [];
+let currentWord = 0;
+let correctKeys = 0;
+let startDate = 0;
+let timer;
+let timerActive = false;
+let punctuation = false;
+let username;
+
+
+getCookie('deck') === '' ? setDeck('mizu') : setDeck(getCookie('deck'));
+getCookie('language') === '' ? setLanguage('english') : setLanguage(getCookie('language'));
+getCookie('wordCount') === '' ? setWordCount(50) : setWordCount(getCookie('wordCount'));
+getCookie('timeCount') === '' ? setTimeCount(60) : setTimeCount(getCookie('timeCount'));
+getCookie('typingMode') === '' ? setTypingMode('wordcount') : setTypingMode(getCookie('typingMode'));
+
+
+function setText(e) {
+  e = e || window.event;
+  var keepWordList = e && e.shiftKey;
+
+  if (!keepWordList) {
+    wordList = [];
+  }
+  currentWord = 0;
+  correctKeys = 0;
+  inputField.value = '';
+  timerActive = false;
+  clearTimeout(timer);
+  textDisplay.style.display = 'block';
+  inputField.className = '';
+
+  switch (typingMode) {
+    case 'wordcount':
+      textDisplay.style.height = 'auto';
+      textDisplay.innerHTML = '';
+      if (!keepWordList) {
+        wordList = [];
+        while (wordList.length < wordCount) {
+          const randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+          if (wordList[wordList.length - 1] !== randomWord || wordList[wordList.length - 1] === undefined || getCookie('language') === 'dots') {
+            wordList.push(randomWord);
+          }
+        }
+      }
+      break;
+
+    case 'time':
+      textDisplay.style.height = '3.2rem';
+      document.querySelector(`#tc-${timeCount}`).innerHTML = timeCount;
+      textDisplay.innerHTML = '';
+      if (!keepWordList) {
+        wordList = [];
+        for (i = 0; i < 500; i++) {
+          let n = Math.floor(Math.random() * randomWords.length);
+          wordList.push(randomWords[n]);
+        }
+      }
+  }
+
+  if (punctuation) addPunctuations();
+  showText();
+  inputField.focus();
+}
+
+function addPunctuations() {
+  if (wordList[0] !== undefined) {
+    
+    wordList[0] = wordList[0][0].toUpperCase() + wordList[0].slice(1);
+
+    for (i = 0; i < wordList.length; i++) {
+      const ran = Math.random();
+      if (i < wordList.length - 1) {
+        if (ran < 0.03) {
+          wordList[i] += ',';
+        } else if (ran < 0.05) {
+          wordList[i] += '.';
+          wordList[i + 1] = wordList[i + 1][0].toUpperCase() + wordList[i + 1].slice(1);
+        } else if (ran < 0.06) {
+          wordList[i] += '?';
+          wordList[i + 1] = wordList[i + 1][0].toUpperCase() + wordList[i + 1].slice(1);
+        } else if (ran < 0.07) {
+          wordList[i] += '!';
+          wordList[i + 1] = wordList[i + 1][0].toUpperCase() + wordList[i + 1].slice(1);
+        } else if (ran < 0.08) {
+          wordList[i] += ';';
+        }
+      }
+    }
+    wordList[wordList.length - 1] += '.';
+
+  }
+}
+
+function showText() {
+  wordList.forEach(word => {
+    let span = document.createElement('span');
+    span.innerHTML = word + ' ';
+    textDisplay.appendChild(span);
+  });
+  textDisplay.firstChild.classList.add('highlight');
+}
+
+inputField.addEventListener('keydown', e => {
+  switch (typingMode) {
+    case 'wordcount':
+      if (currentWord < wordList.length) inputFieldClass();
+    case 'time':
+      if (timerActive) inputFieldClass();
+  }
+  function inputFieldClass() {
+    if (e.key >= 'a' && e.key <= 'z' || (e.key === `'` || e.key === ',' || e.key === '.' || e.key === ';')) {
+      let inputWordSlice = inputField.value + e.key;
+      let currentWordSlice = wordList[currentWord].slice(0, inputWordSlice.length);
+      inputField.className = inputWordSlice === currentWordSlice ? '' : 'wrong';
+    } else if (e.key === 'Backspace') {
+      let inputWordSlice = e.ctrlKey ? '' : inputField.value.slice(0, inputField.value.length - 1);
+      let currentWordSlice = wordList[currentWord].slice(0, inputWordSlice.length);
+      inputField.className = inputWordSlice === currentWordSlice ? '' : 'wrong';
+    } else if (e.key === ' ') {
+      inputField.className = '';
+    }
+  }
+
+  
+  if (currentWord === 0 && inputField.value === '') {
+    switch (typingMode) {
+      case 'wordcount':
+        startDate = Date.now();
+        break;
+
+      case 'time':
+        if (!timerActive) {
+          startTimer(timeCount);
+          timerActive = true;
+        }
+        function startTimer(time) {
+          if (time > 0) {
+            document.querySelector(`#tc-${timeCount}`).innerHTML = time;
+            timer = setTimeout(() => {
+              time--;
+              startTimer(time);
+            }, 1000);
+          } else {
+            timerActive = false;
+            textDisplay.style.display = 'none';
+            inputField.className = '';
+            document.querySelector(`#tc-${timeCount}`).innerHTML = timeCount;
+            showResult();
+          }
+        }
+    }
+  }
+
+  
+  if (e.key === ' ') {
+    e.preventDefault();
+
+    if (inputField.value !== '') {
+  
+      if (typingMode === 'time') {
+        const currentWordPosition = textDisplay.childNodes[currentWord].getBoundingClientRect();
+        const nextWordPosition = textDisplay.childNodes[currentWord + 1].getBoundingClientRect();
+        if (currentWordPosition.top < nextWordPosition.top) {
+          for (i = 0; i < currentWord + 1; i++) textDisplay.childNodes[i].style.display = 'none';
+        }
+      }
+
+    
+      if (currentWord < wordList.length - 1) {
+        if (inputField.value === wordList[currentWord]) {
+          textDisplay.childNodes[currentWord].classList.add('correct');
+          correctKeys += wordList[currentWord].length + 1;
+        } else {
+          textDisplay.childNodes[currentWord].classList.add('wrong');
+        }
+        textDisplay.childNodes[currentWord + 1].classList.add('highlight');
+      } else if (currentWord === wordList.length - 1) {
+        textDisplay.childNodes[currentWord].classList.add('wrong');
+        showResult();
+      }
+
+      inputField.value = '';
+      currentWord++;
+    }
+
+
+  } else if (currentWord === wordList.length - 1) {
+    if (inputField.value + e.key === wordList[currentWord]) {
+      textDisplay.childNodes[currentWord].classList.add('correct');
+      correctKeys += wordList[currentWord].length;
+      currentWord++;
+      showResult();
+    }
+  }
+});
+
+
+function showResult() {
+  let words, minute, acc;
+  switch (typingMode) {
+    case 'wordcount':
+      words = correctKeys / 5;
+      minute = (Date.now() - startDate) / 1000 / 60;
+      let totalKeys = -1;
+      wordList.forEach(e => (totalKeys += e.length + 1));
+      acc = Math.floor((correctKeys / totalKeys) * 100);
+      break;
+
+    case 'time':
+      words = correctKeys / 5;
+      minute = timeCount / 60;
+      let sumKeys = -1;
+      for (i = 0; i < currentWord; i++) {
+        sumKeys += wordList[i].length + 1;
+      }
+      acc = acc = Math.min(Math.floor((correctKeys / sumKeys) * 100), 100);
+  }
+  let wpm = Math.floor(words / minute);
+  document.querySelector('#right-wing').innerHTML = `WPM: ${wpm} / ACC: ${acc}`;
+}
+
+
+document.addEventListener('keydown', e => {
+
+  if (e.altKey || (e.metaKey && e.ctrlKey)) {
+
+    if (e.key === 'd') {
+      setDeck(inputField.value);
+    }
+
+    if (e.key === 'l') {
+      setLanguage(inputField.value);
+    }
+
+    if (e.key === 'm') {
+      setTypingMode(inputField.value);
+    }
+
+    if (e.key === 'p') {
+      setPunctuation(inputField.value);
+    }
+  } else if (!document.querySelector('#deck-center').classList.contains('hidden')) {
+    if (e.key === 'Escape'){
+      hideDeckCenter();
+      inputField.focus();
+    }
+  } else if (e.key === 'Escape') {
+    setText(e);
+  }
+});
+
+function setDeck(_deck) {
+  const deck = _deck.toLowerCase();
+  fetch(`decks/${deck}.css`)
+    .then(response => {
+      if (response.status === 200) {
+        response
+          .text()
+          .then(css => {
+            setCookie('deck', deck, 90);
+            document.querySelector('#deck').setAttribute('href', `decks/${deck}.css`);
+            setText();
+          })
+          .catch(err => console.error(err));
+      } else {
+        console.log(`deck ${deck} is undefine`);
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function setLanguage(_lang) {
+  const lang = _lang.toLowerCase();
+  fetch('texts/random.json')
+    .then(response => response.json())
+    .then(data => {
+      if (typeof data[lang] !== 'undefined') {
+        randomWords = data[lang];
+        setCookie('language', lang, 90);
+
+        setText();
+      } else {
+        console.error(`language ${lang} is undefine`);
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function setTypingMode(_mode) {
+  const mode = _mode.toLowerCase();
+  switch (mode) {
+    case 'wordcount':
+      typingMode = mode;
+      setCookie('typingMode', mode, 90);
+      document.querySelector('#word-count').style.display = 'inline';
+      document.querySelector('#time-count').style.display = 'none';
+      setText();
+      break;
+    case 'time':
+      typingMode = mode;
+      setCookie('typingMode', mode, 90);
+      document.querySelector('#word-count').style.display = 'none';
+      document.querySelector('#time-count').style.display = 'inline';
+      setText();
+      break;
+    default:
+      console.error(`mode ${mode} is undefine`);
+  }
+}
+
+function setPunctuation(_punc) {
+  const punc = _punc.toLowerCase();
+  if (punc === 'true') {
+    punctuation = true;
+    setCookie('punctuation', true, 90);
+    setText();
+  } else if (punc === 'false') {
+    punctuation = false;
+    setCookie('punctuation', false, 90);
+    setText();
+  }
+}
+
+function setWordCount(wc) {
+  setCookie('wordCount', wc, 90);
+  wordCount = wc;
+  document.querySelectorAll('#word-count > span').forEach(e => (e.style.borderBottom = ''));
+  document.querySelector(`#wc-${wordCount}`).style.borderBottom = '2px solid';
+  setText();
+}
+
+function setTimeCount(tc) {
+  setCookie('timeCount', tc, 90);
+  timeCount = tc;
+  document.querySelectorAll('#time-count > span').forEach(e => {
+    e.style.borderBottom = '';
+    e.innerHTML = e.id.substring(3, 6);
+  });
+  document.querySelector(`#tc-${timeCount}`).style.borderBottom = '2px solid';
+  setText();
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
+
+function getCookie(cname) {
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+showAllDecks();
+function showAllDecks(){
+    fetch(`decks/deck-list.json`)
+    .then(response => {
+      if (response.status === 200) {
+        response
+          .text()
+          .then(body => {
+            let decks = JSON.parse(body);
+            let keys = Object.keys(decks);
+            let i;
+            for(i = 0;i < keys.length; i ++){
+
+              let deck = document.createElement('div');
+              deck.setAttribute('class', 'deck-button');
+              deck.setAttribute('onClick', `setDeck('${keys[i]}')`);
+              deck.setAttribute('id', keys[i]);
+
+              
+              deck.setAttribute('tabindex', i + 5);
+              deck.addEventListener('keydown', e => {
+                if (e.key === 'Enter') {
+                  setDeck(deck.id);
+                  inputField.focus();
+
+                }
+              })
+
+              if(decks[keys[i]]['customHTML'] != undefined){
+                deck.style.background = decks[keys[i]]['background'];
+                deck.innerHTML = decks[keys[i]]['customHTML']
+              }else{
+                deck.textContent = keys[i];
+                deck.style.background = decks[keys[i]]['background'];
+                deck.style.color = decks[keys[i]]['color'];
+              }
+              document.getElementById('deck-area').appendChild(deck);
+            }
+          })
+          .catch(err => console.error(err));
+      } else {
+        console.log(`Cant find deck-list.json`);
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+document.getElementById('show-decks').addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    showDeckCenter();
+    inputField.focus();
+  }
+});
+
+document.getElementById('show-scores').addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    showScoreCenter();
+    inputField.focus();
+  }
+});
+
+function showDeckCenter() {
+  document.getElementById('deck-center').classList.remove('hidden');
+  document.getElementById('command-center').classList.add('hidden');
+}
+
+function hideDeckCenter() {
+  document.getElementById('deck-center').classList.add('hidden');
+  document.getElementById('command-center').classList.remove('hidden');
+}
+
+function showScoreCenter() {
+  document.getElementById('score-center').classList.remove('hidden');
+  document.getElementById('command-center').classList.add('hidden');
+}
+
+function hideScoreCenter() {
+  document.getElementById('score-center').classList.add('hidden');
+  document.getElementById('command-center').classList.remove('hidden');
+}
+
+User.getUsers()
+Score.getScores()
